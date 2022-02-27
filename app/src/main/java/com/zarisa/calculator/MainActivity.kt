4a5788e330d137ae955cf59babf4f131729218d6
Zarisa <jannesari.zarisa@gmail.com>
2022-02-27 21:22:28 +0330

    the logic has been changed, it can handle more than one operator, it handles arrangement of operations and handles the config change after rotation

--- app/src/main/java/com/zarisa/calculator/MainActivity.kt
+++ app/src/main/java/com/zarisa/calculator/MainActivity.kt
@@ -1,240 +1,158 @@
 package com.zarisa. calculator
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.Toast
 import com.zarisa.calculator.databinding.ActivityMainBinding
-import java.lang.Exception
+
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
-    var lastNumeric: Boolean = false
-    var stateError: Boolean = false
-    var hasDot: Boolean = false
-    var hasOneOperator:Boolean=false
-    var operator=""
+    private lateinit var binding:ActivityMainBinding
+    private var isOperationAllowed = false
+    private var isDotAllowed = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         val view=binding.root
         setContentView(view)
+        if(savedInstanceState!= null){
+            val configText = savedInstanceState.getString("tv")
+            binding.tv.text = configText
+        }
         onClickListeners()
-        reset()
-    }
-    private fun reset(){
-        lastNumeric= false
-        stateError= false
-        hasDot= false
-        hasOneOperator=false
-        operator=""
     }
     private fun onClickListeners(){
-        binding.buttonDelete?.setOnClickListener { deleteEvent() }
-        binding.buttonEqual?.setOnClickListener { equalEvent() }
-        binding.buttonDiv?.setOnClickListener {operatorEvent(binding.buttonDiv!!)}
-        binding.buttonMul?.setOnClickListener { operatorEvent(binding.buttonMul!!) }
-        binding.buttonSub?.setOnClickListener { operatorEvent(binding.buttonSub!!) }
-        binding.buttonAdd?.setOnClickListener { operatorEvent(binding.buttonAdd!!) }
-        binding.buttonDot?.setOnClickListener { dotEvent() }
-        binding.buttonZero?.setOnClickListener { numberEvent(binding.buttonZero!!) }
-        binding.buttonOne?.setOnClickListener { numberEvent(binding.buttonOne!!) }
-        binding.buttonTwo?.setOnClickListener { numberEvent(binding.buttonTwo!!) }
-        binding.buttonThree?.setOnClickListener { numberEvent(binding.buttonThree!!) }
-        binding.buttonFour?.setOnClickListener { numberEvent(binding.buttonFour!!) }
-        binding.buttonFive?.setOnClickListener { numberEvent(binding.buttonFive!!) }
-        binding.buttonSix?.setOnClickListener { numberEvent(binding.buttonSix!!) }
-        binding.buttonSeven?.setOnClickListener { numberEvent(binding.buttonSeven!!) }
-        binding.buttonEight?.setOnClickListener { numberEvent(binding.buttonEight!!) }
-        binding.buttonNine?.setOnClickListener { numberEvent(binding.buttonNine!!) }
+        binding.buttonDelete.setOnClickListener { deleteEvent() }
+        binding.buttonEqual.setOnClickListener { equalEvent() }
+        binding.buttonDiv.setOnClickListener {operatorEvent("÷")}
+        binding.buttonMul.setOnClickListener { operatorEvent("×") }
+        binding.buttonSub.setOnClickListener { operatorEvent("-") }
+        binding.buttonAdd.setOnClickListener { operatorEvent("+") }
+        binding.buttonDot.setOnClickListener { numberEvent(".") }
+        binding.buttonZero.setOnClickListener { numberEvent("0") }
+        binding.buttonOne.setOnClickListener { numberEvent("1") }
+        binding.buttonTwo.setOnClickListener { numberEvent("2") }
+        binding.buttonThree.setOnClickListener { numberEvent("3") }
+        binding.buttonFour.setOnClickListener { numberEvent("4") }
+        binding.buttonFive.setOnClickListener { numberEvent("5") }
+        binding.buttonSix.setOnClickListener { numberEvent("6") }
+        binding.buttonSeven.setOnClickListener { numberEvent("7") }
+        binding.buttonEight.setOnClickListener { numberEvent("8") }
+        binding.buttonNine.setOnClickListener { numberEvent("9") }
     }
+    private fun deleteEvent() {
+        binding.tv.text = ""
+        isOperationAllowed = false
+        isDotAllowed = true
+        Toast.makeText(this,"DELETE", Toast.LENGTH_SHORT).show()
+    }
+    private fun numberEvent(value:CharSequence) {
+        if (value == ".") {
+            if (isOperationAllowed&&isDotAllowed){
+                binding.tv.append(value)
+                isDotAllowed = false
+                isOperationAllowed = false
+                Toast.makeText(this,value, Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            binding.tv.append(value)
+            isOperationAllowed = true
+            Toast.makeText(this,value, Toast.LENGTH_SHORT).show()
+        }
 
-    private fun numberEvent(view: View){
-        Toast.makeText(this,(view as Button).text, Toast.LENGTH_SHORT).show()
-        if (stateError) {
-            // If current state is Error, replace the error message
-            binding.tv?.text=""
-            binding.tv?.text = view.text.toString()
-            stateError = false
-        } else {
-            // If not, already there is a valid expression so append to it
-            binding.tv?.text="${binding.tv?.text}${view.text.toString()}"
-        }
-        // Set the flag
-        lastNumeric = true
     }
-    private fun dotEvent(){
-        if (lastNumeric && !stateError && !hasDot) {
-            Toast.makeText(this,".", Toast.LENGTH_SHORT).show()
-            binding.tv?.text="${binding.tv?.text}."
-            lastNumeric = false
-            hasDot = true
+    private fun operatorEvent(operator:CharSequence) {
+        if (isOperationAllowed) {
+            Toast.makeText(this,operator, Toast.LENGTH_SHORT).show()
+            binding.tv.append(operator)
+            isOperationAllowed = false
+            isDotAllowed = true
         }
     }
-    private fun operatorEvent(view: View){
-        if (lastNumeric && !stateError&&!hasOneOperator) {
-            Toast.makeText(this,(view as Button).text, Toast.LENGTH_SHORT).show()
-            operator=view.text.toString()
-            binding.tv?.text="${binding.tv?.text}${view.text}"
-            lastNumeric = false
-            hasDot = false    // Reset the DOT flag
-            hasOneOperator=true
+    private fun equalEvent() {
+        val firstFilter = detachmentTheDigitsAndOperators()
+        val mulDivFilter = calMulDiv(firstFilter)
+        when {
+            firstFilter.isEmpty() -> {
+                binding.tv.text=""
+            }
+            mulDivFilter.isEmpty() -> {
+                binding.tv.text=""
+            }
+            else -> {
+                binding.tv.text=calAddSub(mulDivFilter).toString()
+            }
         }
+        Toast.makeText(this,"=", Toast.LENGTH_SHORT).show()
     }
-    private fun equalEvent(){
-        try {
-            // If the current state is error, nothing to do.
-            // If the last input is a number only, solution can be found.
-            if (lastNumeric && !stateError) {
-                Toast.makeText(this, "=", Toast.LENGTH_SHORT).show()
-                if (!hasOneOperator && !hasDot) {
-                    binding.tv?.text = (binding.tv?.text as Number).toDouble().toString()
-                }
-                if (hasOneOperator) {
-                    hasOneOperator = false
-                    var text = binding.tv?.text?.split(operator)
-                    var a=(text?.get(1) as? Number)?.toDouble()
-                    var b=(text?.get(2) as? Number)?.toDouble()
-                    hasDot = true // Result contains a dot
-                    when (operator) {
-                        "+" -> binding.tv?.text = (a!!.plus(b!!)).toString()
-//                            (calculatorClass.addition(a,b)).toString()
-                        "-" -> binding.tv?.text = (calculatorClass.subtraction(a,b)).toString()
-                        "×" -> binding.tv?.text = (calculatorClass.multiplication(a,b)).toString()
-                        "÷" -> {
-                            if (b==0.0) {
-                                binding.tv?.text = "Error"
-                                stateError = true
-                                lastNumeric = false
-                            } else {
-                                binding.tv?.text = (calculatorClass.division(a,b)).toString()
-                            }
+    private fun detachmentTheDigitsAndOperators(): MutableList<Any> {
+        val list = mutableListOf<Any>()
+        var currentDigit = ""
+        for (element in binding.tv.text) {
+            if (element.isDigit()||element == '.')
+                currentDigit += element
+            else {
+                list.add(currentDigit.toFloat())
+                currentDigit = ""
+                list.add(element)
+            }
+        }
+        if (currentDigit != "")
+            list.add(currentDigit.toFloat())
+
+        return list
+    }
+    private fun calAddSub(passedList: MutableList<Any>): Float {
+        var result = passedList[0] as Float
+        for (i in passedList.indices){
+            if (passedList[i] is Char && i != passedList.lastIndex){
+                val operator = passedList[i]
+                val nextDigit = passedList[i+1] as Float
+                if(operator =='+')
+                    result +=nextDigit
+                if(operator =='-')
+                    result -=nextDigit
+            }
+        }
+        return result
+    }
+    private fun calMulDiv(passedList: MutableList<Any>): MutableList<Any> {
+        var list = passedList
+        while( list.contains('×')||list.contains('÷')){
+            val temporaryList = mutableListOf<Any>()
+            var lengthOfList = list.size
+            for (i in list.indices){
+                if( list[i] is Char && i != list.lastIndex && i < lengthOfList){//list[i] is the operation we should work on
+                    val numberBeforeOperator = list[i-1] as Float
+                    val numberAfterOperator = list[i+1] as Float
+                    when(list[i]){
+                        '×' ->
+                        {
+                            temporaryList.add(numberBeforeOperator*numberAfterOperator)
+                            lengthOfList = i+1
+                        }
+                        '÷' ->
+                        {
+                            temporaryList.add(numberBeforeOperator/numberAfterOperator)
+                            lengthOfList = i+1
+                        }
+                        else ->
+                        {
+                            temporaryList.add(numberBeforeOperator)
+                            temporaryList.add(list[i])
                         }
                     }
                 }
+                if (i > lengthOfList )
+                    temporaryList.add(list[i])
             }
-        }catch (e:Exception){
-            binding.tv?.text = "Error"
+            list = temporaryList
         }
-    }
-    private fun deleteEvent(){
-        Toast.makeText(this,"DELETE", Toast.LENGTH_SHORT).show()
-        binding.tv?.text=""
-        reset()
-        calculatorClass.resetTheMemory()
+        return  list
     }
 
-
-
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString("tv" , binding.tv.text.toString())
+        super.onSaveInstanceState(outState)
+    }
 }
-//with library
-//    // Represent whether the lastly pressed key is numeric or not
-//    var lastNumeric: Boolean = false
-//
-//    // Represent that current state is in error or not
-//    var stateError: Boolean = false
-//
-//    // If true, do not allow to add another DOT
-//    var lastDot: Boolean = false
-
-//    fun numberEvent(view:View){
-//        Toast.makeText(this,(view as Button).text, Toast.LENGTH_SHORT).show()
-//        if (stateError) {
-//            // If current state is Error, replace the error message
-//            binding.tv?.text = (view as Button).text
-//            stateError = false
-//        } else {
-//            // If not, already there is a valid expression so append to it
-//            binding.tv?.text="${binding.tv?.text}${(view as Button).text}"
-//        }
-//        // Set the flag
-//        lastNumeric = true
-//    }
-
-//    fun dotEvent(view:View){
-//        Toast.makeText(this,".", Toast.LENGTH_SHORT).show()
-//        if (lastNumeric && !stateError && !lastDot) {
-//            binding.tv?.text="${binding.tv?.text}."
-//            lastNumeric = false
-//            lastDot = true
-//        }
-//    }
-
-
-//    fun operatorEvent(view: View) {
-//        Toast.makeText(this,(view as Button).text, Toast.LENGTH_SHORT).show()
-//        if (lastNumeric && !stateError) {
-//            binding.tv?.text="${binding.tv?.text}${(view as Button).text}"
-//            lastNumeric = false
-//            lastDot = false    // Reset the DOT flag
-//        }
-//    }
-
-//    fun deleteEvent(view:View){
-//        Toast.makeText(this,"DELETE", Toast.LENGTH_SHORT).show()
-//        binding.tv?.text=""
-//        lastNumeric = false
-//        stateError = false
-//        lastDot = false
-//    }
-
-//    fun equalEvent(view: View) {
-//        Toast.makeText(this,"=", Toast.LENGTH_SHORT).show()
-//        // If the current state is error, nothing to do.
-//        // If the last input is a number only, solution can be found.
-//        if (lastNumeric && !stateError) {
-//            // Read the expression
-//            val txt = binding.tv?.text.toString()
-//            // Create an Expression (A class from exp4j library)
-//            val expression = ExpressionBuilder(txt).build()
-//            try {
-//                // Calculate the result and display
-//                val result = expression.evaluate()
-//                binding.tv?.text = result.toString()
-//                lastDot = true // Result contains a dot
-//            } catch (ex: Exception) {
-//                // Display an error message
-//                binding.tv?.text = "Error"
-//                stateError = true
-//                lastNumeric = false
-//            }
-//        }
-//    }
-
-
-
-
-
-
-
-//    unClickAbles()
-//    onClickListeners()
-//    private fun onClickListeners(){
-//        binding.buttonDelete?.setOnClickListener {
-//            hasDot=false
-//            binding.tv?.text=""
-//            memory=0.0
-//            toastMaker("DELETE")
-//            unClickAbles()
-//        }
-//        binding.buttonDiv?.setOnClickListener {
-//            addValueToTV(resources.getString(R.string.divide_sign))
-//        }
-//    }
-//    private fun toastMaker(s:String){
-//        Toast.makeText(this,s, Toast.LENGTH_SHORT).show()
-//    }
-//    fun addValueToTV(s:String){
-//        binding.tv?.text="${binding.tv?.text}$s"
-//    }
-//    private fun unClickAbles(){
-//        if(binding.tv?.text==""){
-//            binding.buttonDiv?.isClickable=false
-//            binding.buttonMul?.isClickable=false
-//        }
-//        if (hasDot){
-//            binding.buttonDot?.isClickable=false
-//        }
-//    }
